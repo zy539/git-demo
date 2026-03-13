62c65733fd541ba639503cc95d2d0e546f4dc651
zy359 <3352670846@qq.com>
2026-03-13 10:21:27 +0800

    Feedback.java

---
+++ Personal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Personal {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int number =sc.nextInt();
+        int ge =number;
+        
+    }
+    
+}
